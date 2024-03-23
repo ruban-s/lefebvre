1f2845a7e57bf629af56369b35b34fc922190ff4
iamrithu <ritimahesh29@ghmail.com>
2024-03-23 13:37:53 +0530

    deploy to vercel

--- components/admin/home/home.tsx
+++ components/admin/home/home.tsx
@@ -10,17 +10,17 @@ const HomeScreen = () => {
 
   return (
     <main className="w-full h-full bg-white overflow-auto p-10">
-      <main className="w-full h-auto grid justify-center items-center  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  gap-6">
+      <main className="w-full h-auto grid justify-center items-center  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3  gap-6">
         {adminTabs.map(({ icon: Icon, ...info }: TabData, index: number) => {
           return (
             <Link
               key={index}
               href={info.link}
-              className={`w-full h-[220px] flex flex-row md:flex-col justify-evenly items-center p-2 ${
+              className={`w-full h-[70px] sm:h-[100px]  md:h-[220px] ld:h-[220px] flex flex-row md:flex-col justify-center  space-x-2   md:justify-evenly items-center p-2 ${
                 path === info.link && "ring-1"
               } bg-white shadow-md  cursor-pointer rounded-lg hover:ring-1 hover:ring-theme`}>
-              <div className="p-6 bg-blue-50 rounded-full">
-                <Icon className="text-theme text-[40px]" />
+              <div className="p-2 sm:p-3 md:p-4 lg:p-6 bg-blue-50 rounded-full">
+                <Icon className="text-theme  text-[20px] sm:text-[25px] md:text-[40px]" />
               </div>
               <p className="text-theme font-bold">{info.label}</p>
             </Link>
