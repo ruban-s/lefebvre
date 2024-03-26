c684a8c8fd0186c7b20847bdf417b249fb64ae54
iamrithu <ritimahesh29@ghmail.com>
2024-03-26 17:42:32 +0530

    --change login bg image--

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 const ProductedLayout = ({ children }: ProductedLayoutProps) => {
   return (
     <main className="w-full h-full flex flex-row justify-center items-center">
-      <div className="   relative flex flex-col md:w-full h-full bg-[url('/cog-bg-3.jpeg')] bg-cover justify-center items-center ">
+      <div className="   relative flex flex-col md:w-full h-full bg-[url('/cog-bg3.jpeg')] bg-cover justify-center items-center ">
         {children}
       </div>
     </main>
